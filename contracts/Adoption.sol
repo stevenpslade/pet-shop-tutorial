892ca6a62fe32dc1e071ae9796f062d05d3861c0
stevenpslade <stevenpslade@gmail.com>
2018-02-22 22:55:35 -0800

    first commit and starting Adoption contract

---
+++ contracts/Adoption.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.17;
+
+contract Adoption {
+  address[16] public adopters;
+
+  // Adopting a pet
+  function adopt(uint _pedId) public returns (uint) {
+    require(_pedId >= 0 && _pedId <= 15);
+    adopters[_petId] = msg.sender;
+    return _pedId;
+  }
+}
